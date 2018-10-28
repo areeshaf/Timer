162184a378607362ec4caa3a05687c37b3b59cb4
areeshaFatima <areeshaf7@gmail.com>
2018-10-28 11:21:15 +0530

    timer

--- app/src/main/java/com/example/hp/timerapp/MainActivity.java
+++ app/src/main/java/com/example/hp/timerapp/MainActivity.java
@@ -4,27 +4,26 @@ import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     MediaPlayer mediaPlayer;
+    TextView textView;
+    SeekBar timeSeekBar;
+    boolean btnpressed=ture;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void buttonPressed(View view){
 
 
-        SeekBar timeSeekBar=(SeekBar)findViewById(R.id.seekBar);
-
-        timeSeekBar.setMax(60000);
-
-        new CountDownTimer(60000,1000){
+        new CountDownTimer(timeSeekBar.getProgress()*1000+100,1000){
             @Override
             public void onTick(long l) {
 
+                updateTime((int)l/1000);
             }
 
             @Override
@@ -33,5 +32,49 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.start();
             }
         }.start();
+    }
+
+    public void updateTime(int sec){
+        int minute=sec/60;
+        int seconds=sec-(minute*60);
+
+        String secString=Integer.toString(seconds);
+        if(seconds<=9){
+            secString="0"+secString;
+        }
+
+        textView.setText(Integer.toString(minute)+":"+secString);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textView=(TextView)findViewById(R.id.textView);
+        timeSeekBar=(SeekBar)findViewById(R.id.seekBar);
+
+        timeSeekBar.setMax(600);
+        timeSeekBar.setProgress(30);
+
+        timeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                updateTime(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
